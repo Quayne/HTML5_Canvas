da10aed012ebd3867458cf3e8d4122f3a4f1b02d
Quayne <quayne.brown@mohawkcollege.ca>
2016-02-17 10:48:01 -0500

    mouseover and mouse out is now completed in main.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,43 @@
 $(document).ready(function () {
+
     var selectedArea;
-    $("area").click(function () {
-        
-        //var id = $(this).attr('id');
-        //remove hightlight on all areas
-        highlightArea(this);
-        selectedArea = this;
-    });
+    //$("area").click(function () {
+
+    //    //var id = $(this).attr('id');
+    //    //remove hightlight on all areas
+    //    highlightArea(this);
+    //    selectedArea = this;
+    //});
 
     $("area").mouseover(function () {
-        //var id = $(this).attr('id');
-        $(this).toggle(function () {
-            highlightArea(this);
-        });
+        highlightArea(this);
     });
 
     $("area").mouseout(function () {
-        //var id = $(this).attr('id');
-
-        //only remove if it is not the selectedArea
-        $(this).toggle(function () {
-            removeHighlight(this);
-        });
+        removeHighlight(this);
     });
 });
 
+
+//Clear highlighted area
 function removeHighlight(element) {
     var canvas = document.getElementById("b");
     var context = canvas.getContext("2d");
     context.clearRect(0, 0, 494, 494);
 }
 
+//Highlight area
 function highlightArea(element) {
-    //var $x = "#" + id;
     var coords = $(element).prop("coords").split(",");
-
-    var canvas =  document.getElementById("b");
+    var canvas = document.getElementById("b");
     var context = canvas.getContext("2d");
-    
 
+    context.beginPath();
     context.moveTo(coords[0], coords[1]);
     for (i = 2; i < coords.length; i += 2) {
         context.lineTo(coords[i], coords[i + 1]);
     }
+    context.closePath();
     context.strokeStyle = "#eee";
     context.fillStyle = 'red';
     context.stroke();
